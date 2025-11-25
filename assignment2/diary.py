ccb62b9cd5a82d694f8800432a196ac350c671e7
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-11-24 18:56:03 -0700

    Update: Task 1 finished the tassk loop and write

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -2,8 +2,9 @@
 
 with open('diary.txt', 'a') as file:
     first_line = input("What happened today?")
+    file.write(first_line + '\n')
     while True:
         second_line = input("What else?")
+        file.write(second_line + '\n')
         if second_line.lower() == 'done for now':
             break
-    file.write(first_line + second_line + '\n')
