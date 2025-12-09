d5c80d20edaa785f2e6ed10f40b45cd2135d1f5c
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-09 06:56:40 -0700

    Initial: Task 1

---
+++ assignment3/log-decorator.py
@@ -0,0 +1,29 @@
+# Task 1
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+def logger_decorator(func):
+    def wrapper(*args, **kwargs):
+        logger = logging.getLogger(func.__name__ + "_parameter_log")
+        logger.setLevel(logging.INFO)
+        logger.addHandler(logging.FileHandler("./decorator.log", "a"))
+        print(f"function {func.__name__}")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+@logger_decorator
+def string_func():
+    return "Hello,World!"
+
+def positional_func(name, year):
+    print(f"positional parameters: {name},{year}")
+    return True
+
+def other_func(**kwargs):
+    print(f"keyword parameters: {kwargs}")
+    return logger_decorator
+
+string_func()
+positional_func("Iris", 3)
+other_func(name="bob", status="active", hobby="shopping")
