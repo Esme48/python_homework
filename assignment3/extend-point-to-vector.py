d830c6ed6196eb21e685512ad7894c03546a85f4
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-09 22:02:25 -0700

    Initial: Task 5

---
+++ assignment3/extend-point-to-vector.py
@@ -0,0 +1,15 @@
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self):
+        return
+    
+    def __str__(self):
+        return
+    
+    def euclidean(self):
+        return
+
