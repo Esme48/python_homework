6e7e5f0b01686dc9842490def80e15bbbd7179bf
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-09 21:11:20 -0700

    Update: Task 4 split the correctors in the word into a list and added the if/else

--- assignment3/hangman-closure.py
+++ assignment3/hangman-closure.py
@@ -4,11 +4,16 @@ def make_hangman(secret_word):
     guesses = []
 
     def hangman_closure(letter):
+        if letter in secret_word:
+            print("You guessed a letter!")
+        else:
+            print("Please try again!")
         guesses.append(letter)
         return hangman_closure
     return hangman_closure
 
-secret_value = input("What is your secret word:").lower().split()
+
+secret_value = list(input("What is your secret word:").lower())
 hang_man = make_hangman(secret_value)
 
 while True:
