f2ec0d907032c3f654c633041977e4d282a98380
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-09 21:43:47 -0700

    Update: Task 4 added an if/not statement to correctly append the array

--- assignment3/hangman-closure.py
+++ assignment3/hangman-closure.py
@@ -4,12 +4,10 @@ def make_hangman(secret_word):
     guesses = []
 
     def hangman_closure(letter):
-        if letter in secret_word:
-            print("You guessed a letter!")
-        else:
-            print("Please try again!")
-        guesses.append(letter)
-        return hangman_closure
+        if letter not in guesses:
+            guesses.append(letter)
+        output_value = " ".join([value if value in guesses else "_" for value in secret_word])
+        return f"Word So Far: {output_value}"
     return hangman_closure
 
 
@@ -19,5 +17,11 @@ hang_man = make_hangman(secret_value)
 while True:
     letter_value = input("What is your letter guess:").lower()
     hang_man(letter_value)
+
+
+        # if letter in secret_word:
+        #     print("You guessed a letter!")
+        # else:
+        #     print("Please try again!")
     
 
