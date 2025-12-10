42d93b29e3bb83252178cc8c7fd2410afa435bd4
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-09 20:40:36 -0700

    Update: Task 4 changed where the input is being asked for

--- assignment3/hangman-closure.py
+++ assignment3/hangman-closure.py
@@ -1,11 +1,12 @@
 
 
 def make_hangman(secret_word):
-    secret_value = input("What is your word:")
     guesses = []
 
     def hangman_closure(letter):
-        guess_value = input("What is the letter that you are guessing:")
         guesses.append(letter)
         return hangman_closure
-    return 
+    return make_hangman
+
+hang_man = make_hangman(print(input("What is your secret word:").lower().split()))
+
