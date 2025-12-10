f556e202711eb2d942b96855e3067266b99adf64
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-09 20:49:31 -0700

    Update: Task 4 added the input component

--- assignment3/hangman-closure.py
+++ assignment3/hangman-closure.py
@@ -8,5 +8,11 @@ def make_hangman(secret_word):
         return hangman_closure
     return make_hangman
 
-hang_man = make_hangman(print(input("What is your secret word:").lower().split()))
+secret_value = input("What is your secret word:").lower().split()
+hang_man = make_hangman(secret_value)
+
+while True:
+    letter_value = input("What is your letter guess:").lower()
+    hang_man(letter_value)
+    break
 
