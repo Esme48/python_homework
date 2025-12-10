ee22209c28a6b133964b5421969031acfaccb9f0
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-10 13:21:42 -0700

    Initial: Task 6 foundation

---
+++ assignment3/tictactoe.py
@@ -0,0 +1,10 @@
+
+
+class TictactoeException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+class Board:
+    def __init__(self):
+        return 
