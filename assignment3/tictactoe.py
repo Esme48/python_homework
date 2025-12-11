108ea43d65f68517e4ed5fbe2b051699fd7dfea8
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-10 17:17:06 -0700

    Update: Task 5 inital testing of matrix

--- assignment3/tictactoe.py
+++ assignment3/tictactoe.py
@@ -7,4 +7,12 @@ class TictactoeException(Exception):
 
 class Board:
     def __init__(self):
-        return 
+        self.board_array = [["" for _ in range(3)] for _ in range(3)]
+        
+
+game_board = Board()
+print(f"This is the ouput: {game_board}")
+
+
+# Notes: 
+# - https://stackoverflow.com/questions/65729276/when-creating-lists-it-seems-that-forms-like-false-3-3-only-copies-the
