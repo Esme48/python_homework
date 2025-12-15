58b5d31e50b93784e97c864a304d9aaed37557d4
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-14 21:12:16 -0700

    Initial: Task 1 foundation

---
+++ assignment4/assignment4.py
@@ -0,0 +1,11 @@
+## Task 1
+
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [24, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+df = pd.DataFrame(data)
+print(df)
