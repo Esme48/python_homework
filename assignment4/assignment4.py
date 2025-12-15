7d41c94c27c07f49681a49f70bcf81db15cec66b
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-14 21:24:05 -0700

    Update: Task 1 added salary column to the data

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -9,3 +9,9 @@ data = {
 }
 task1_data_frame = pd.DataFrame(data)
 print(task1_data_frame)
+
+task1_with_salary = task1_data_frame.copy()
+
+salary = ['70000', '80000', '90000']
+task1_with_salary['Salary'] = salary
+print(task1_with_salary)
