70e389d7f7aa553e30a27d7d3a594fbb8ac2152d
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-16 13:41:35 -0700

    Update: Task 4 Salary update

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -71,6 +71,9 @@ clean_data.drop_duplicates(inplace=True)
 clean_data["Age"] = clean_data["Age"].replace("unknown", pd.NA)
 clean_data["Age"] = pd.to_numeric(clean_data["Age"], errors = "coerce")
 #print(clean_data)
+clean_data["Salary"] = clean_data["Salary"].replace("unknown", pd.NA)
+clean_data["Salary"] = pd.to_numeric(clean_data["Salary"], errors = "coerce")
+
 age_mean = clean_data["Age"].mean()
 salary_median = clean_data["Salary"].median()
 
