783929f052d8bbec8eb3a10355a951cf8d42f492
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-16 10:52:43 -0700

    Update: Task 1 changed salary values to integer instead of string

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -12,7 +12,7 @@ print(task1_data_frame)
 
 task1_with_salary = task1_data_frame.copy()
 
-salary = ['70000', '80000', '90000']
+salary = [70000, 80000, 90000]
 task1_with_salary['Salary'] = salary
 print(task1_with_salary)
 
