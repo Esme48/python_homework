2bcbc2685f9c0653a0a3b4c945162ef5607ebdd8
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-16 11:06:41 -0700

    Update: Task 1 added csv file writing component

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -19,3 +19,8 @@ print(task1_with_salary)
 task1_older = task1_with_salary.copy()
 task1_older['Age'] = task1_older['Age'] + 1
 print(task1_older)
+
+task1_older.to_csv("employees.csv", index=False)
+
+
+
