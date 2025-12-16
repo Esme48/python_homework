813a221a75af1e394e7ade3cfe82ea9eabd568f5
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-12-16 11:30:51 -0700

    Update: Task 2 created json file and read json file

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -1,6 +1,7 @@
 ## Task 1
 
 import pandas as pd
+import json
 
 data = {
     'Name': ['Alice', 'Bob', 'Charlie'],
@@ -26,3 +27,24 @@ task1_older.to_csv("employees.csv", index=False)
 #Task 2
 task2_employees = pd.read_csv('employees.csv')
 print(task2_employees)
+
+data_two = {
+    'Name': ['Eve', 'Frank'],
+    'Age': [28, 40],
+    'City': ['Miami', 'Seattle'],
+    'Salary' : [60000, 95000]
+}
+
+json_str = json.dumps(data_two, indent = 4)
+
+with open("additional_employees.json", "w") as f:
+    f.write(json_str)
+
+json_employees = pd.read_json('additional_employees.json')
+print(json_employees)
+
+#Notes: 
+#- https://www.geeksforgeeks.org/python/reading-and-writing-json-to-a-file-in-python/
+# Create a JSON file (additional_employees.json). The file adds two new employees. Eve, who is 28, lives in Miami, and has a salary of 60000, and Frank, who is 40, lives in Seattle, and has a salary of 95000.
+# Load this JSON file into a new DataFrame and assign it to the variable json_employees.
+# Print the DataFrame to verify it loaded correctly and run the tests.
